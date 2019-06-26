7b68cdb14d93a355921d560496a33e0e5d3a4a7e
Augusto Santos <AGtaugusto92@hotmail.com>
2019-06-26 15:50:35 -0300

    Tela Cadastro Serie, Tela cadastro Exercicio na Serie

---
+++ Boora_TCC_2019/Boora_TCC_2019/TELAS_CADASTRO/Cadastrar_Exercicio_Serie.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Boora_TCC_2019.MODEL;
+using Boora_TCC_2019.DAO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.ObjectModel;
+
+namespace Boora_TCC_2019.TELAS_CADASTRO
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Cadastrar_Exercicio_Serie : ContentPage
+	{
+        Aluno aluno;
+        Serie serie;
+        Exercicio exercicio;
+        ExercicioDAO exercicioDAO = new ExercicioDAO();
+        private List<Exercicio> listaPesquisa { get; set; }
+        private List<Exercicio> listaInterna { get; set; }
+        ObservableCollection<Exercicio> listaExerciciosSerie = new ObservableCollection<Exercicio>();
+        private List<Exercicio> listaAuxiliar { get; set; }
+        public Cadastrar_Exercicio_Serie (Aluno a, Serie s)
+		{
+			InitializeComponent ();
+            
+            aluno = a;
+            serie = s;
+		}
+
+        private async void Btn_Cadastrar_Exercicios_Serie(object sender, EventArgs e)
+        {
+            Exercicios_Serie_DAO exercicios_Serie_DAO = new Exercicios_Serie_DAO();
+            Exercicios_Serie exercicios_Serie = new Exercicios_Serie();
+
+            exercicios_Serie.Id_Exercicios_Serie = Convert.ToInt32(Txt_Id_Exercicios_Serie.Text);
+            //PASSA O ID DA SERIE PARA CADASTRO DO EXERCICIO
+            exercicios_Serie.Id_Serie = serie.Id_Serie;
+            exercicios_Serie.Qtd_repeticoes = Convert.ToInt32(Txt_Quantidade_repeticoes.Text);
+            exercicios_Serie.Qtd_Vezes = Convert.ToInt32(Txt_Quantidade_Vezes.Text);
+            exercicios_Serie.Tempo_Minimo = txt_tempo_minimo.Text;
+            exercicios_Serie.Tempo_Maximo = txt_Tempo_Maximo.Text;
+            exercicios_Serie.Tempo_Execucao = txt_tempo_execucao.Text;
+            // await exercicios_Serie_DAO.Cadastrar_Exercicios_Serie(exercicios_Serie);
+
+            listaAuxiliar = listaInterna.Where(a => a.Id_exercicio == (exercicios_Serie.Id_Exercicios_Serie)).ToList();
+            listaExerciciosSerie.Add(listaAuxiliar[0]);
+            ListaExerciciosSeriesView.ItemsSource = listaExerciciosSerie;
+            ListaExerciciosSeriesView.IsRefreshing = true;
+
+
+
+            limpaCampos();
+        }
+       
+        public void limpaCampos()
+        {
+            Txt_Id_Exercicios_Serie.Text = "";
+            Txt_Quantidade_repeticoes.Text = "";
+            Txt_Quantidade_Vezes.Text = "";
+            txt_tempo_minimo.Text = "";
+            txt_Tempo_Maximo.Text = "";
+            txt_tempo_execucao.Text = "";
+            Txt_Quantidade_repeticoes.Text = "";
+
+            
+            
+
+        }
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+           
+            listaInterna = await exercicioDAO.Busca_Exercicio();
+
+            for (int i = 0; i < listaInterna.Count; i++)
+            {
+                string path = await exercicioDAO.Buscar_IMAGEM(listaInterna[i].Imagem_Gif);
+
+                listaInterna[i].Imagem_Gif = path;
+
+            }
+
+            ListaExercicios.ItemsSource = listaInterna;
+            
+                
+            
+            
+
+
+        }
+
+        private async void BuscaRapida(Object sender, TextChangedEventArgs args)
+        {
+            listaInterna = await exercicioDAO.Busca_Exercicio();
+            try
+            {
+                listaPesquisa = listaInterna.Where(a => a.Nome.Contains(args.NewTextValue)).ToList();
+
+                for (int i = 0; i < listaInterna.Count; i++)
+                {
+                    string path = await exercicioDAO.Buscar_IMAGEM(listaInterna[i].Imagem_Gif);
+                    listaInterna[i].Imagem_Gif = path;
+
+                }
+
+                ListaExercicios.ItemsSource = listaPesquisa;
+
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        private void SelecaoExercicioAction(object sender, SelectedItemChangedEventArgs args)
+        {
+            exercicio = (Exercicio)args.SelectedItem;
+            Txt_nome_Exercicio.Text = exercicio.Nome;
+            Txt_Id_Exercicios_Serie.Text = exercicio.Id_exercicio.ToString();
+        }
+    }
+}
--- Boora_TCC_2019/Boora_TCC_2019/TELAS_CADASTRO/Cadastrar_Serie.xaml.cs
+++ Boora_TCC_2019/Boora_TCC_2019/TELAS_CADASTRO/Cadastrar_Serie.xaml.cs
@@ -17,12 +17,14 @@ namespace Boora_TCC_2019.TELAS
         string dataFim;
         SerieDAO serieDAO = new SerieDAO();
         Exercicios_Serie_DAO exercicios_Serie_DAO = new Exercicios_Serie_DAO();
-
+        Aluno aluno;
         //GAMBIARA AQUI PRA ID DA SERIE A SER CADASTRADA VAI TER. PROBLME AQUI SE FOR CADASTRAR EXERCICIO SEPADO NA SERIE AI TEM QUE COLOCAR O VALOR DO ID 
         public static int Id_serie_Cadastrar_Exercicio;
-        public Cadastrar_Serie(Aluno aluno)
+        public Cadastrar_Serie(Aluno a)
         {
+           
             InitializeComponent();
+            aluno = a;
             Lbl_data_Inicial.Text =  DateTime.Now.ToString("dd/MM/yyyy");
             Txt_Id_Do_Aluno.Text = (aluno.Id_Aluno.ToString());
             Txt_nome_Serie.Text = aluno.Nome;
@@ -54,34 +56,36 @@ namespace Boora_TCC_2019.TELAS
                await DisplayAlert("Data não Selecionada", "Selecione data", "OK");
             }
            
-            await serieDAO.Cadastrar_Serie(serie);
-        }
-        private async void Btn_Cadastrar_Exercicios_Serie(object sender, EventArgs e)
-        {
-            Exercicios_Serie exercicios_Serie = new Exercicios_Serie();
+            //await serieDAO.Cadastrar_Serie(serie);
 
-            exercicios_Serie.Id_Exercicios_Serie = Convert.ToInt32(Txt_Id_Exercicios_Serie.Text);
-            //PASSA O ID DA SERIE PARA CADASTRO DO EXERCICIO
-            exercicios_Serie.Id_Serie = Id_serie_Cadastrar_Exercicio;
-            exercicios_Serie.Qtd_repeticoes = Convert.ToInt32(Txt_Quantidade_repeticoes.Text);
-            exercicios_Serie.Qtd_Vezes = Convert.ToInt32(Txt_Quantidade_Vezes.Text);
-            exercicios_Serie.Tempo_Minimo = txt_tempo_minimo.Text;
-            exercicios_Serie.Tempo_Maximo = txt_Tempo_Maximo.Text;
-            exercicios_Serie.Tempo_Execucao = txt_tempo_execucao.Text;
-            await exercicios_Serie_DAO.Cadastrar_Exercicios_Serie(exercicios_Serie);
-
-            limpaCampos();
+            await Navigation.PushAsync(new TELAS_CADASTRO.Cadastrar_Exercicio_Serie(aluno, serie));    
         }
-        public void limpaCampos()
-        {
-            Txt_Id_Exercicios_Serie.Text = "";
-            Txt_Quantidade_repeticoes.Text="";
-            Txt_Quantidade_Vezes.Text = "";
-            txt_tempo_minimo.Text = "";
-            txt_Tempo_Maximo.Text = "";
-            txt_tempo_execucao.Text = "";
-            Txt_Quantidade_repeticoes.Text = "";
+        //private async void Btn_Cadastrar_Exercicios_Serie(object sender, EventArgs e)
+        //{
+        //    Exercicios_Serie exercicios_Serie = new Exercicios_Serie();
 
-        }
+        //    exercicios_Serie.Id_Exercicios_Serie = Convert.ToInt32(Txt_Id_Exercicios_Serie.Text);
+        //    //PASSA O ID DA SERIE PARA CADASTRO DO EXERCICIO
+        //    exercicios_Serie.Id_Serie = Id_serie_Cadastrar_Exercicio;
+        //    exercicios_Serie.Qtd_repeticoes = Convert.ToInt32(Txt_Quantidade_repeticoes.Text);
+        //    exercicios_Serie.Qtd_Vezes = Convert.ToInt32(Txt_Quantidade_Vezes.Text);
+        //    exercicios_Serie.Tempo_Minimo = txt_tempo_minimo.Text;
+        //    exercicios_Serie.Tempo_Maximo = txt_Tempo_Maximo.Text;
+        //    exercicios_Serie.Tempo_Execucao = txt_tempo_execucao.Text;
+        //    await exercicios_Serie_DAO.Cadastrar_Exercicios_Serie(exercicios_Serie);
+
+        //    limpaCampos();
+        //}
+        //public void limpaCampos()
+        //{
+        //    Txt_Id_Exercicios_Serie.Text = "";
+        //    Txt_Quantidade_repeticoes.Text="";
+        //    Txt_Quantidade_Vezes.Text = "";
+        //    txt_tempo_minimo.Text = "";
+        //    txt_Tempo_Maximo.Text = "";
+        //    txt_tempo_execucao.Text = "";
+        //    Txt_Quantidade_repeticoes.Text = "";
+
+        //}
     }
 }
