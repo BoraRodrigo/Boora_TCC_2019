9b09fa7b60bcb8ec554c11433568da499fa85a50
BoraRodrigo <rodrigobora93@gmail.com>
2019-08-30 15:59:19 -0300

    -Criei uma email - arrumei o icone do insta e do whats.

--- Boora_TCC_2019/Boora_TCC_2019/ClassesUTEIS/Email.cs
+++ Boora_TCC_2019/Boora_TCC_2019/ClassesUTEIS/Email.cs
@@ -21,16 +21,16 @@ namespace Boora_TCC_2019.ClassesUTEIS
                           .ToArray());
             return sua_senha;
         }
-        public async Task RedefinirSenha(string emailEnviarsenha,string nomeAcademia)
+        public async Task RedefinirSenha(string emailEnviarsenha, string nomeAcademia)
         {
             senha_para_alterar = gerarsenha();
-            AlunoDAO alunoDAO= new AlunoDAO();
+            AlunoDAO alunoDAO = new AlunoDAO();
 
-            string remetenteEmail = "rodrigobora93@gmail.com"; //O e-mail do remetente
+            string remetenteEmail = "booraappcontato@gmail.com"; //O e-mail do remetente
             MailMessage mail = new MailMessage();
             mail.To.Add(emailEnviarsenha);
             mail.From = new MailAddress(remetenteEmail, "Aplicativo Boora", System.Text.Encoding.UTF8);
-            mail.Subject = "Assunto: Redefinição de senha Borra!";
+            mail.Subject = "Assunto: AMO VOCE!";
             mail.SubjectEncoding = System.Text.Encoding.UTF8;
             mail.Body = "Olá sua nova senha de acesso é: " + senha_para_alterar;
             mail.BodyEncoding = System.Text.Encoding.UTF8;
@@ -38,7 +38,7 @@ namespace Boora_TCC_2019.ClassesUTEIS
             mail.Priority = MailPriority.High; //Prioridade do E-Mail
 
             SmtpClient client = new SmtpClient();  //Adicionando as credenciais do seu e-mail e senha:
-            client.Credentials = new System.Net.NetworkCredential(remetenteEmail, "Aa@222abcdegrb36");
+            client.Credentials = new System.Net.NetworkCredential(remetenteEmail, "qwe123QWE!@#");
 
             client.Port = 587; // Esta porta é a utilizada pelo Gmail para envio
             client.Host = "smtp.gmail.com"; //Definindo o provedor que irá disparar o e-mail
@@ -46,7 +46,7 @@ namespace Boora_TCC_2019.ClassesUTEIS
             try
             {
                 client.Send(mail);
-                await   alunoDAO.Aterar_senha(emailEnviarsenha,senha_para_alterar,nomeAcademia);
+                await alunoDAO.Aterar_senha(emailEnviarsenha, senha_para_alterar, nomeAcademia);
             }
             catch (Exception ex)
             {
